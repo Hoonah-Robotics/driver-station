9ca43f851f5b2e35f308e11b3680fdf1b28b5721
Hoonah-Robotics <73606540+Hoonah-Robotics@users.noreply.github.com>
2020-11-14 19:12:59 -0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -48,24 +48,24 @@ basic.forever(function () {
         basic.clearScreen()
         comment.comment("throttle by tilt forwards/backwards")
         comment.comment("flat = 0; forwards = -180; backwards = 180")
-        comment.comment("\"x -1\" to make forwards = 180")
-        pitch = input.rotation(Rotation.Pitch) * -1
-        if (pitch > -10 && pitch < 10) {
+        pitch = input.rotation(Rotation.Pitch)
+        if (pitch > -5 && pitch < 5) {
             comment.comment("dead band on pitch so nearly flat is still STOPPED (otherwise robot will creep)")
             throttle = 0
         } else {
             comment.comment("filter noisy accelerometer")
-            throttle = Math.constrain(pitch, -40, 40)
+            comment.comment("\"x -1\" to make forwards = 180")
+            throttle = Math.map(Math.constrain(pitch, -40, 40), -40, 40, 255, -255)
         }
         comment.comment("turn by tilt left/right")
         comment.comment("flat = 0; left tilt = -180; right tilt = 180")
         roll = input.rotation(Rotation.Roll)
-        if (roll > -10 && roll < 10) {
+        if (roll > -5 && roll < 5) {
             comment.comment("dead band on roll so nearly flat is still straight (otherwise robot won't drive straight)")
             turn = 0
         } else {
             comment.comment("filter noisy accelerometer")
-            turn = Math.constrain(roll, -40, 40)
+            turn = Math.map(Math.constrain(roll, -40, 40), -40, 40, -255, 255)
         }
         radio.sendValue("y", throttle * power)
         radio.sendValue("x", turn * power)
@@ -75,26 +75,26 @@ basic.forever(function () {
         } else {
             led.plot(0, 4)
         }
-        if (throttle >= 40) {
+        if (throttle >= 200) {
             led.plot(2, 0)
             led.plot(2, 1)
-        } else if (throttle > 20) {
+        } else if (throttle > 100) {
             led.plot(2, 1)
-        } else if (throttle <= -40) {
+        } else if (throttle <= -200) {
             led.plot(2, 3)
             led.plot(2, 4)
-        } else if (throttle < -20) {
+        } else if (throttle < -100) {
             led.plot(2, 3)
         }
-        if (turn >= 40) {
+        if (turn >= 200) {
             led.plot(3, 2)
             led.plot(4, 2)
-        } else if (turn > 20) {
+        } else if (turn > 100) {
             led.plot(3, 2)
-        } else if (turn <= -40) {
+        } else if (turn <= -200) {
             led.plot(1, 2)
             led.plot(0, 2)
-        } else if (turn < -20) {
+        } else if (turn < -100) {
             led.plot(1, 2)
         }
     }
